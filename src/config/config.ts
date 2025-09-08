dbf72b3001f772e252e19cd0b9d594c81bce5083
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2025-09-08 20:25:06 +0600

    update package

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,15 +1,20 @@
 import dotenv from "dotenv";
+import path from "path";
 
-dotenv.config();
+dotenv.config({ path: path.join(process.cwd(), ".env") });
 
-interface Config {
-  port: number;
-  nodeEnv: string;
-}
-
-const config: Config = {
-  port: Number(process.env.PORT) || 3000,
-  nodeEnv: process.env.NODE_ENV || "development",
+export default {
+  env: process.env.NODE_ENV,
+  port: process.env.PORT || 3000,
+  database_url: process.env.DATABASE_URL,
+  default_student_pass: process.env.DEFAULT_STUDENT_PASS,
+  default_faculty_pass: process.env.DEFAULT_FACULTY_PASS,
+  default_admin_pass: process.env.DEFAULT_ADMIN_PASS,
+  bycrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    refresh_secret: process.env.JWT_REFRESH_SECRET,
+    expires_in: process.env.JWT_EXPIRES_IN,
+    refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
+  },
 };
-
-export default config;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,36 @@
-import { config } from "dotenv";
+import { Server } from "http";
 import app from "./app";
+import config from "./config/config";
+import { errorlogger, logger } from "./shared/logger";
 
-app.listen(config.port, () => {
-  console.log(`Server running on port ${config.port}`);
-});
+async function bootstrap() {
+  const server: Server = app.listen(config.port, () => {
+    logger.info(`Server running on port ${config.port}`);
+  });
+
+  const exitHandler = () => {
+    if (server) {
+      server.close(() => {
+        logger.info("Server closed");
+      });
+    }
+    process.exit(1);
+  };
+
+  const unexpectedErrorHandler = (error: unknown) => {
+    errorlogger.error(error);
+    exitHandler();
+  };
+
+  process.on("uncaughtException", unexpectedErrorHandler);
+  process.on("unhandledRejection", unexpectedErrorHandler);
+
+  process.on("SIGTERM", () => {
+    logger.info("SIGTERM received");
+    if (server) {
+      server.close();
+    }
+  });
+}
+
+bootstrap();
